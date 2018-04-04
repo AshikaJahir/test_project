95f811abd5a83b42cc249027317b2ebb47e97137
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-04 16:26:17 +0530

    atempted multiple queries for reference
    in the view function u have practiced queries, and commented them

--- app/Http/Controllers/register.php
+++ app/Http/Controllers/register.php
@@ -36,7 +36,7 @@ class register extends Controller
        $email_id = $request->email_id;
        
         //inserting a record 
-        //DB::table('users')->insert(['first_name' => $first_name , 'last_name'=> $last_name,'gender' => $gender,'age'=>$age,'email_id' => $email_id,'unique_id'=>'Laravel']);
+        //\DB::table('crud_table')->insert(['first_name' => $first_name , 'last_name'=> $last_name,'gender' => $gender,'age'=>$age,'email_id' => $email_id,'unique_id'=>'Laravel']);
      
         $id = \DB::table('crud_table')->insertGetId(['first_name' => $first_name , 'last_name'=> $last_name,
             'gender' => $gender,'age'=>$age,'email_id' => $email_id,'unique_id'=>'Laravel']);
@@ -66,9 +66,12 @@ class register extends Controller
         \Log::channel('single')->info('The user has deleted the student with id' .$id); //For specified channel
     }
     
+    //u have tried many experimental queries in this view function and commented them 
     public function view()
     {
        
+        //start of project's view code  
+
         //using model
         
        /* $crud = \App\crud_table::all();
@@ -82,8 +85,16 @@ class register extends Controller
         
         $students = \DB::table('crud_table')->select('id','first_name','last_name','age','gender','email_id')->get();
         
-       
-        $students = json_encode($students);
+        //return view('studentView', ['students' => $students]);
+        //
+        //To display the columns seperately
+        /*foreach($students as $student)
+            {
+                echo '<br>'. $student->id.'<br'.$student->first_name . '<br>' .$student->last_name . '<br>' .$student->age . '<br>' . $student->gender . '<br>' .$student->email_id;
+                echo '<br>';
+            }*/
+        
+        //$students = json_encode($students);
         echo $students;        
         
         echo "<br><br><br>";
@@ -94,6 +105,103 @@ class register extends Controller
        \Log::channel('single')->info('The user has viewed the student details'); //For specified channel
         echo "<a href='/laravel/test_project/public'>Home</a>";
         
+        echo '<br>';
+        //end of project's view code
+     
+        
+        /*//Checking multiple select queries
+        //1.Retrieving a single row
+        $user = \DB::table('crud_table')->where('first_name', 'Anu')->first();
+        echo $user->first_name. '<br>' . $user->last_name; echo '<br>';
+        
+        //2.Retrieving a single column of the row        
+        $email = \DB::table('crud_table')->where('first_name', 'Anu')->value('email_id');
+        echo $email;echo '<br>';
+        
+        //3.Retrieving a column from the table
+        $id_list = \DB::table('crud_table')->pluck('id');
+
+            foreach ($id_list as $id) {
+                echo $id . '<br>';
+            }
+            
+         //4.For chunking
+         \DB::table('crud_table')->orderBy('first_name')->chunk(5, function ($student_list) {
+            foreach ($student_list as $student) {
+                echo $student->id;
+            }
+            });
+            
+           //5.Using Aggregate function
+            $student_count = \DB::table('crud_table')->count();
+            echo '<br>'.$student_count.'<br>';
+
+            $last_id = \DB::table('crud_table')->max('id');
+            echo $last_id.'<br>';
+            
+            //6.Combining clauses
+            $ge_count = \DB::table('crud_table')
+                ->where('first_name', 'ge')
+                ->count('id');
+            echo $ge_count.'<br>';
+            
+            //7.Checking the existence of the record returns 1 on true null on false
+            $record_exist = \DB::table('crud_table')->where('first_name', 'Anu')->exists();
+            echo $record_exist.'<br>';
+            $record_doesnot_exist = \DB::table('crud_table')->where('first_name', 'Anucudv')->doesntExist();
+            echo $record_doesnot_exist.'<br>';
+            
+            //8.Selecting specified columns
+            $student_list = \DB::table('crud_table')->select('first_name', 'email_id as user_email')->get();
+            foreach ($student_list as $student) {
+                echo $student->user_email;
+            }
+            
+            //9.Getting Distinct values based on entire row
+            $student_list = \DB::table('crud_table')->distinct()->get();
+            echo $student_list;
+            
+            //10.Getting distinct values based on single column
+            $student_list = \DB::table('crud_table')->select('first_name')->distinct()->get();
+            echo $student_list;
+            
+            //11.Using where clause for two or more conditions
+            $student = \DB::table('crud_table')->where([
+                            ['first_name', '=', 'anu'],
+                            ['last_name', '=', 'menon'],
+                        ])->get();
+                foreach ($student as $student) {
+                   echo $student->id.'<br>'; 
+                }
+            //12.Using where clause for or condition
+            $students = \DB::table('crud_table')
+                    ->where('id', '>', 60)
+                    ->orWhere('first_name', 'ge')
+                    ->get();
+            echo $students;
+            
+            //13.Retrieving where using between
+            $students = \DB::table('crud_table')
+                    ->whereBetween('id', [60, 65])->get();
+            echo '<br><br>'.$students;
+            
+            //14.Retrieving where using notbetween
+            $students = \DB::table('crud_table')
+                    ->whereNotBetween('id', [60, 65])->get();
+            echo '<br><br>'.$students;
+            
+            //15. retrieving where in
+            $students = \DB::table('crud_table')
+                    ->whereIn('id', [70, 60])
+                    ->get();
+            echo '<br><br>'.$students;
+            
+            //16. retrieving where in
+            $students = \DB::table('crud_table')
+                    ->whereNotIn('id', [70, 60])
+                    ->get();
+            echo '<br><br>'.$students;*/
+            
     }
       public function update(Request $request)
     {
