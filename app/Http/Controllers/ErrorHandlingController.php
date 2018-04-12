b364de72138c7df952a21c6e7a75545a85dacb82
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-12 12:36:50 +0530

    custom Exception does not work in controller

---
+++ app/Http/Controllers/ErrorHandlingController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exceptions\QueryException;
+
+
+
+class ErrorHandlingController extends Controller
+{
+    public function check()
+    {
+     
+        try
+        {
+            \DB::table('crud')->where('email_id','$email_id')->first();
+        } catch (Exception $e) {
+            throw new QueryException($e->getMessage());
+        }
+           
+             
+      
+    }
+    
+    
+}
