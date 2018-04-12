3d14f2787e53dc33b3994736b26ff7b74246af58
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-12 13:11:58 +0530

    Custom Report method inbuilt

---
+++ app/Exceptions/CustomInbuiltReportException.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Exceptions;
+
+use Exception;
+
+class CustomInbuiltReportException extends Exception //\Exception denotes base class
+{
+    public function report()//base class function , here it is overrided
+    {
+        dd('custominbuilt report exception');
+    }
+}
